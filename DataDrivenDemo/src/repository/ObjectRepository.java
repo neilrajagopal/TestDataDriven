c3102bab0e94d88cbc0c409718eaf094fb706a19
NeelWebApp <neelamegam.rengasamy@gmail.com>
2020-07-29 06:59:45 +0530

    Initial commit

---
+++ DataDrivenDemo/src/repository/ObjectRepository.java
@@ -0,0 +1,18 @@
+package repository;
+
+import org.openqa.selenium.By;
+
+public class ObjectRepository {
+	//NEw Tours
+	public static By nttxtUserName = By.name("userName");
+	public static By nttxtPassword = By.name("password");
+	public static By ntbtnLogin = By.name("login");
+	public static By ntlnkSignOff = By.linkText("SIGN-OFF");
+
+	//Guru99
+	public static By txtUserName = By.name("uid");
+	public static By txtPassword = By.name("password");
+	public static By btnLogin = By.name("btnLogin");
+	public static By lnkSignOff = By.linkText("Log out");
+
+}
